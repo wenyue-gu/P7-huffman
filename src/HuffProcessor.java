6f5bd53e29c2562cf4d897d09ce24093a333521a
Calleigh Smith <calleigh.smith@duke.edu>
2019-12-01 20:51:35 -0500

    Started decompression

--- src/HuffProcessor.java
+++ src/HuffProcessor.java
@@ -1,3 +1,4 @@
+import java.io.IOException;
 
 /**
  * Although this class has a history of several years,
@@ -64,12 +65,38 @@ public class HuffProcessor {
 		if (magic != HUFF_TREE) {
 			throw new HuffException("invalid magic number "+magic);
 		}
-		out.writeBits(BITS_PER_INT,magic);
+		HuffNode root = readTree(in);
+		HuffNode current = root;
 		while (true){
-			int val = in.readBits(BITS_PER_WORD);
-			if (val == -1) break;
-			out.writeBits(BITS_PER_WORD, val);
+			int bits = in.readBits(1);
+			if (bits==-1) throw new HuffException("bad input, no PSEUDO_EOF");
+			else{
+				if(bits==0) current = current.myLeft;
+				else{ current=current.myRight;}
+				if(current.myLeft==null && current.myRight==null){
+					if(current.myValue==PSEUDO_EOF) break;
+					else {
+						out.writeBits(BITS_PER_WORD, bits);
+						current = root;
+					}
+				}
+			}
 		}
 		out.close();
+	}
+
+	private HuffNode readTree(BitInputStream in) {
+		int bit = in.readBits(1);
+		if(bit==-1) throw new HuffException("reached end of sequence");
+		if(bit==0){
+			HuffNode left = readTree(in);
+			HuffNode right = readTree(in);
+			return new HuffNode(0,0,left,right);
+		}
+		else{
+			int value = in.readBits(BITS_PER_WORD+1);
+			return new HuffNode(value, 0, null, null);
+		}
+
 	}
 }
