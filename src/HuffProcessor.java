cdfae0227390cd060d6c16c45b074d74fbbeb082
Wenyue Gu <wenyue.gu@duke.edu>
2019-12-03 22:04:22 -0500

    x

--- src/HuffProcessor.java
+++ src/HuffProcessor.java
@@ -1,4 +1,6 @@
 import java.io.IOException;
+import java.util.PriorityQueue;
+import java.util.TreeMap;
 
 /**
  * Although this class has a history of several years,
@@ -7,7 +9,7 @@ import java.io.IOException;
  * <P>
  * Changes include relying solely on a tree for header information
  * and including debug and bits read/written information
- * 
+ *
  * @author Owen Astrachan
  */
 
@@ -15,20 +17,20 @@ public class HuffProcessor {
 
 	public static final int BITS_PER_WORD = 8;
 	public static final int BITS_PER_INT = 32;
-	public static final int ALPH_SIZE = (1 << BITS_PER_WORD); 
+	public static final int ALPH_SIZE = (1 << BITS_PER_WORD);
 	public static final int PSEUDO_EOF = ALPH_SIZE;
 	public static final int HUFF_NUMBER = 0xface8200;
 	public static final int HUFF_TREE  = HUFF_NUMBER | 1;
 
 	private final int myDebugLevel;
-	
+
 	public static final int DEBUG_HIGH = 4;
 	public static final int DEBUG_LOW = 1;
-	
+
 	public HuffProcessor() {
 		this(0);
 	}
-	
+
 	public HuffProcessor(int debug) {
 		myDebugLevel = debug;
 	}
@@ -43,13 +45,90 @@ public class HuffProcessor {
 	 */
 	public void compress(BitInputStream in, BitOutputStream out){
 
-		while (true){
-			int val = in.readBits(BITS_PER_WORD);
-			if (val == -1) break;
-			out.writeBits(BITS_PER_WORD, val);
-		}
+		int[] counts = readForCounts(in);
+		HuffNode root = makeTreeFromCounts(counts);
+		String[] codings = makeCodingsFromTree(root);
+		out.writeBits(BITS_PER_INT, HUFF_TREE);
+		writeHeader(root, out);
+		in.reset();
+		writeCompressedBits(codings, in, out);
 		out.close();
 	}
+
+	private void writeHeader(HuffNode root, BitOutputStream out) {
+		if(root.myRight==null&&root.myLeft==null){
+			out.writeBits(1, 1);
+			out.writeBits(BITS_PER_WORD+1, root.myValue);
+		}
+		else{
+			out.writeBits(1, 0);
+			writeHeader(root.myLeft, out);
+			writeHeader(root.myRight, out);
+		}
+	}
+
+	private void writeCompressedBits(String[] codings, BitInputStream in, BitOutputStream out) {
+		while(true){
+			int val = in.readBits(BITS_PER_WORD);
+			if(val==-1){
+				String code = codings[PSEUDO_EOF];
+				out.writeBits(code.length(), Integer.parseInt(code,2));
+				break;
+			}
+			String code = codings[val];
+			out.writeBits(code.length(), Integer.parseInt(code, 2));
+		}
+	}
+
+	private String[] makeCodingsFromTree(HuffNode root) {
+		String[] encodings = new String[ALPH_SIZE+1];
+		codingHelper(root, "", encodings);
+		return encodings;
+	}
+	private void codingHelper(HuffNode root, String path, String[] encodings) {
+		if(root==null) return;
+		if(root.myLeft==null&&root.myRight==null){
+			encodings[root.myValue]=path;
+			return;
+		}
+		else{
+			codingHelper(root.myLeft,path+"0",encodings);
+			codingHelper(root.myRight,path+"1",encodings);
+			return;
+		}
+	}
+
+	private HuffNode makeTreeFromCounts(int[] counts) {
+		PriorityQueue<HuffNode> pq = new PriorityQueue<>();
+		for(int k=0;k<counts.length;k++){
+			if(counts[k]>0){
+				pq.add(new HuffNode(k, counts[k], null, null));
+			}
+		}
+		while(pq.size()>1){
+			HuffNode left = pq.remove();
+			HuffNode right = pq.remove();
+			HuffNode t = new HuffNode(-1, left.myWeight+right.myWeight, left, right);
+			pq.add(t);
+
+		}
+		HuffNode root = pq.remove();
+		return root;
+	}
+
+	private int[] readForCounts(BitInputStream in) {
+		int[] freq = new int[ALPH_SIZE+1];
+		while(true){
+			int val = in.readBits(BITS_PER_WORD);
+			if(val==-1){
+				freq[PSEUDO_EOF]=1;
+				break;
+			}
+			freq[val]++;
+		}
+		return freq;
+	}
+
 	/**
 	 * Decompresses a file. Output file must be identical bit-by-bit to the
 	 * original.
@@ -73,7 +152,7 @@ public class HuffProcessor {
 			else{
 				if(bits==0) current = current.myLeft;
 				else{ current=current.myRight;}
-				if(current.myLeft==null && current.myRight==null){
+				if(current.myLeft==null&&current.myRight==null){
 					if(current.myValue==PSEUDO_EOF) break;
 					else {
 						out.writeBits(BITS_PER_WORD, current.myValue);
